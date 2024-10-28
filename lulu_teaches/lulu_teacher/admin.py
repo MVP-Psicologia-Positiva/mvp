d9f20a8c64d89426db650f8120d8bb3f6d48a837
Lucas <lrsmello.lucas@gmail.com>
2024-10-28 07:40:36 -0300

    rest_api created

--- lulu_teaches/lulu_teacher/admin.py
+++ lulu_teaches/lulu_teacher/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
 from django.contrib import admin
 
-from .models import historicalSessions
+from .models import historicalSessions, UsersMilestones, luluTrainning
 
 admin.site.register(historicalSessions)
+admin.site.register(UsersMilestones)
+admin.site.register(luluTrainning)
